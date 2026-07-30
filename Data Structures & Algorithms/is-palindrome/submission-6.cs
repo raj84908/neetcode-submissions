55531346965ed54b8b916e7b06109f993fe63c60
raj84908 <85078816+raj84908@users.noreply.github.com>
2026-07-30 11:44:16 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        string AlphaNumericString = new string(s.Where(char.IsLetterOrDigit).Select(char.ToLower).ToArray());
+        int first = 0;
+        int end = AlphaNumericString.Count()-1;
+
+        while(end > first)
+        {
+            if(AlphaNumericString[first] == AlphaNumericString[end])
+            {
+                first++;
+                end--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+
+
+    }
+}
