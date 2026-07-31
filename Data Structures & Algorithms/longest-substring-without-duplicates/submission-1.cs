0b41e9444ee13119e094880214a63bd78e6610a8
raj84908 <85078816+raj84908@users.noreply.github.com>
2026-07-31 13:00:27 -0400

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        if(string.IsNullOrEmpty(s))
+        {
+            return 0;
+        }
+
+        int LongestSequence = 1;
+        HashSet<char> HashChar = new HashSet<char>();
+
+        for(int i =0; i < s.Count(); i++)
+        {
+            HashChar.Clear();
+            char CurrentChar = s[i];
+            HashChar.Add(CurrentChar);
+            int TempSequenceCount = 1;
+            for(int j = i+1; j < s.Count(); j++)
+            {
+                if(!HashChar.Contains(s[j]))
+                {
+                    TempSequenceCount++;
+                    HashChar.Add(s[j]);
+                }
+                else{
+                    break;
+                }
+                    
+                    
+            }
+            if(TempSequenceCount > LongestSequence)
+            {
+                LongestSequence = TempSequenceCount;
+            }
+        }
+
+        return LongestSequence;
+
+    }
+}
