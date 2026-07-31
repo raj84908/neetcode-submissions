72060eb4625f9df61558a65c2569b752869cf551
raj84908 <85078816+raj84908@users.noreply.github.com>
2026-07-31 12:34:24 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+
+        int Profit = 0;  
+        for(int i =0; i < prices.Count(); i++)
+        {
+            int Buy = prices[i];
+            for(int j = i; j < prices.Count(); j++)
+            {
+                int Sell = prices[j];
+                int PotentialProfit = Sell - Buy;
+                if(PotentialProfit > Profit)
+                {
+                    Profit = PotentialProfit;
+                }
+            }
+        }
+
+        return Profit;
+
+    }
+}
